7da414ab0ad218887547c8f1dfbb5b5b4802f72e
Paul <alphaceves@gmail.com>
2021-08-20 22:54:05 -0500

    se agrego cardlist y se removio los componentes de appjs

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,10 @@
 import React from 'react';
 import './app.scss';
-import ATexto from './components/textarea/textarea';
-import Boton from './components/button/button';
-import Input from './components/input/input';
-import Nav from './components/nav/nav';
-import Footer from './components/footer/footer';
 
 function App() {
   return (
     <div className="App">
-      <ATexto label='Tu mensaje'/>
-
-      <Boton texto='Cotiza aquí'/>
-      <Input 
-        type="text"
-        label="Nombre"
-      />
-      <Input 
-        type="email"
-        label="Correo electronico"
-      />
-
-      <Nav/>
-
-      <Footer/>
-
+      GusGay
     </div>
   );
 }
