60fd4d62a4759ec500d006227607aa68ffe4581f
Paul <alphaceves@gmail.com>
2021-08-20 22:34:13 -0500

    remove empty space

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './app.scss';
 import ATexto from './components/textarea/textarea';
-
 import Boton from './components/button/button';
 import Input from './components/input/input';
 import Nav from './components/nav/nav';
