a56769bd0448fdd7a8f2bec80eacd6c3f8fa17f4
arlex12 <labarlex12@gmail.com>
2021-08-22 01:38:16 -0500

    Footer olas
    Desarrollo del footer de olas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import './app.scss';
 import ATexto from './components/textarea/textarea';
+<<<<<<< Updated upstream
+=======
+
+import Boton from './components/button/button';
+import Input from './components/input/input';
+import Nav from './components/nav/nav';
+import Footer from './components/footer/footer';
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
       <ATexto label='Tu mensaje'/>
+<<<<<<< Updated upstream
+=======
+
+      <Boton texto='Cotiza aquí'/>
+      <Input 
+        type="text"
+        label="Nombre"
+      />
+      <Input 
+        type="email"
+        label="Correo electronico"
+      />
+
+      <Nav/>
+
+      <Footer/>
+
+>>>>>>> Stashed changes
     </div>
   );
 }
