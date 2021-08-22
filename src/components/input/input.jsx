55d8d1875b64a3ad24c0de3784bfb3722800f70a
guzhdz <guzhdz28@gmail.com>
2021-08-22 14:19:53 -0500

    Establecimiento de font
    Se establecio el font-family y font-size predeterminados

--- src/components/input/input.jsx
+++ src/components/input/input.jsx
@@ -3,7 +3,7 @@ import './_input.scss'
 
 const Input = ({type, label}) => (
     <div className="cont-input">
-        <input type={type} required/>
+        <input type={type} placeholder={label} required/>
         <label>{label}</label>
     </div>
 );
