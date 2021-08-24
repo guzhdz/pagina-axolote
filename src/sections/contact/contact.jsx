c3ff3271931e98616747fd07aa110f3c8cb63232
guzhdz <guzhdz28@gmail.com>
2021-08-24 07:28:25 -0500

    Estilado Contacto
    Se dio estilo a la seccion de contacto como tambien al componente button, ademas se arreglo un error visual de la seccion Home

--- src/sections/contact/contact.jsx
+++ src/sections/contact/contact.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import Input from '../../components/input/input'
 import Textarea from '../../components/textarea/textarea'
+import Button from '../../components/button/button'
+import './_contact.scss'
 
 const Contact = () => (
     <div className="cont-contact">
         <div className="container">
-            <Input type="text" label="Nombre"/>
-            <Input type="email" label="Correo"/>
-            <Textarea label="Escribe tu mensaje aqui"/>
+            <h2>Contacto</h2>
+            <div className="form">
+                <Input type="text" label="Nombre"/>
+                <Input type="email" label="Correo"/>
+                <Textarea label="Escribe tu mensaje aqui" className="textarea"/>
+                <Button texto="Enviar" />
+            </div>
         </div>
     </div>
 );
