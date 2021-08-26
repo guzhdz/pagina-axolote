80af9b8c41b8518342f326f0fe17bfd0438f3f46
arlex12 <labarlex12@gmail.com>
2021-08-25 22:34:15 -0500

    parallax

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import './app.scss';
+<<<<<<< Updated upstream
 import ATexto from './components/textarea/textarea';
 import Boton from './components/button/button';
 import Input from './components/input/input';
 import Nav from './components/nav/nav';
 import Footer from './components/footer/footer';
+=======
+import Nav from './components/nav/nav.jsx';
+import Home from './sections/home/home.jsx';
+import Services from './sections/services/services.jsx';
+import Contact from './sections/contact/contact.jsx';
+import Footer from './components/footer/footer.jsx';
+import Parallax from './components/parallax/parallax.jsx'
+>>>>>>> Stashed changes
 
 function App() {
   return (
     <div className="App">
+<<<<<<< Updated upstream
       <ATexto label='Tu mensaje'/>
 
 
@@ -26,6 +36,17 @@ function App() {
 
       <Footer/>
 
+=======
+      <div id="Inicio"/>
+      <Nav />
+      <Home />
+      <div id="Servicios"/>
+      <Services />
+      <div id="Contacto"/>
+      <Contact />
+      <Parallax />
+      <Footer />
+>>>>>>> Stashed changes
     </div>
   );
 }
---
+++ src/components/parallax/parallax.js
@@ -0,0 +1,29 @@
+import './parallax.jsx';
+import './_parallax.scss';
+import $ from 'jquery';
+ 
+$(document).ready(function() {
+ 
+let text = document.getElementById('text');
+let bird1 = document.getElementById('bird1');
+let bird2 = document.getElementById('bird2');
+let rocks = document.getElementById('rocks');
+let forest = document.getElementById('forest');
+let water = document.getElementById('water');
+let ajolote = document.getElementById('ajolote');
+ 
+window.addEventListener('scroll', function(){
+    let value = window.scrollY -2500;
+
+        text.style.top = 2 + value * -0.01  + '%';
+        bird1.style.top = value * -0.3 + 'px';
+        bird1.style.left = value * 0.6 + 'px';
+        bird2.style.top = value * -0.2 + 'px';
+        bird2.style.left = value * -1 + 'px';
+        rocks.style.top = value * -0.12 + 'px';
+        forest.style.top = value * 0.15 + 'px';
+        ajolote.style.top = value * -0.8 + 'px';
+        ajolote.style.left = value * -1 + 'px';
+});
+
+});
