1ae28989c073c5e67077356f95aea3cff004449e
Paul <alphaceves@gmail.com>
2021-08-28 23:09:44 -0500

    paralax fixed

--- src/components/parallax/parallax.js
+++ src/components/parallax/parallax.js
@@ -9,12 +9,11 @@ let bird1 = document.getElementById('bird1');
 let bird2 = document.getElementById('bird2');
 let rocks = document.getElementById('rocks');
 let forest = document.getElementById('forest');
-let water = document.getElementById('water');
  
 window.addEventListener('scroll', function(){
     let value = window.scrollY;
 
-        text.style.top = 2 + value * -0.02  + '%';
+        text.style.top = value * -0.02  + '%';
         bird1.style.top = value * -0.3 + 'px';
         bird1.style.left = value * 0.6 + 'px';
         bird2.style.top = value * -0.2 + 'px';
