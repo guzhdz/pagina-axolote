efc6c7674e4274452876a7b8ce3bc26d51e5b6c8
arlex12 <labarlex12@gmail.com>
2021-08-28 23:52:51 -0500

    bye bye parallax hola home

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,14 @@ import Nav from './components/nav/nav.jsx';
 import Services from './sections/services/services.jsx';
 import Contact from './sections/contact/contact.jsx';
 import Footer from './components/footer/footer.jsx';
-import Parallax from './components/parallax/parallax.jsx';
+import Home from './sections/home/home.jsx'
 
 function App() {
   return (
     <div className="App">
       <div id="Inicio"/>
       <Nav />
-      <Parallax />
+      <Home />
       <div id="Servicios"/>
       <Services />
       <div id="Contacto"/>
