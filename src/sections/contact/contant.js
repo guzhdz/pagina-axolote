cdab4c38b103c45b60818c6273fd0c9a4b6a93b6
guzhdz <guzhdz28@gmail.com>
2021-08-30 22:02:56 -0500

    Posibles aimaciones de aparicion

--- src/sections/contact/contant.js
+++ src/sections/contact/contant.js
@@ -2,12 +2,15 @@ import $ from 'jquery';
 $(document).ready(function() {
     const title = document.getElementById("title");
     const form = document.getElementById("form");
+    const border = document.getElementById("title").parentElement;
     const observeItems = [title, form];
-    
+
     const elemObservado = (entries) => {
         entries.forEach(elem => {
-            if(elem.isIntersecting)
+            if(elem.isIntersecting) {
                 elem.target.classList.add("animation--appear");
+                border.classList.add("animation--appear2");
+            }
         });
     }
 
